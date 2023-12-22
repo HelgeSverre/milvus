42e76671db302a98410c09724e8a81724e31e95c
Helge Sverre <helge.sverre@gmail.com>
2023-12-22 15:09:33 +0100

    aider: Implemented missing properties in `src/Requests/VectorOperations/QueryVector.php` for the "/v1/vector/query" endpoint.
    # Aider chat conversation:
    
    USER: implement the missing properties in `src/Requests/VectorOperations/QueryVector.php` using the "/v1/vector/query" endpoint in the `openapi.json`, reference `CreateCollection` to see how similar files are implemented
    
    ASSISTANT: ```diff
    --- src/Requests/VectorOperations/QueryVector.php
    +++ src/Requests/VectorOperations/QueryVector.php
    @@ ... @@
     class QueryVector extends Request implements HasBody
     {
         use HasJsonBody;
    
         protected Method $method = Method::POST;
    
    +    protected string $collectionName;
    +    protected string $filter;
    +    protected ?int $limit = null;
    +    protected ?int $offset = null;
    +    protected ?array $outputFields = null;
    +    protected ?string $dbName = null;
    
         public function resolveEndpoint(): string
         {
             return '/v1/vector/query';
         }
    
         public function __construct(
    -        protected string $publicEndpoint,
    +        protected string $publicEndpoint, // The endpoint of your cluster.
    +        string $collectionName,
    +        string $filter,
    +        ?int $limit = null,
    +        ?int $offset = null,
    +        ?array $outputFields = null,
    +        ?string $dbName = null
         ) {
    +        $this->collectionName = $collectionName;
    +        $this->filter = $filter;
    +        $this->limit = $limit;
    +        $this->offset = $offset;
    +        $this->outputFields = $outputFields;
    +        $this->dbName = $dbName;
         }
    +
    +    protected function defaultBody(): array
    +    {
    +        return array_filter([
    +            'collectionName' => $this->collectionName,
    +            'filter' => $this->filter,
    +            'limit' => $this->limit,
    +            'offset' => $this->offset,
    +            'outputFields' => $this->outputFields,
    +            'dbName' => $this->dbName,
    +        ]);
    +    }
     }
    ```

--- src/Requests/VectorOperations/QueryVector.php
+++ src/Requests/VectorOperations/QueryVector.php
@@ -18,13 +18,44 @@ class QueryVector extends Request implements HasBody
 
     protected Method $method = Method::POST;
 
+    protected string $collectionName;
+    protected string $filter;
+    protected ?int $limit = null;
+    protected ?int $offset = null;
+    protected ?array $outputFields = null;
+    protected ?string $dbName = null;
+
     public function resolveEndpoint(): string
     {
         return '/v1/vector/query';
     }
 
     public function __construct(
-        protected string $publicEndpoint,
+        protected string $publicEndpoint, // The endpoint of your cluster.
+        string $collectionName,
+        string $filter,
+        ?int $limit = null,
+        ?int $offset = null,
+        ?array $outputFields = null,
+        ?string $dbName = null
     ) {
+        $this->collectionName = $collectionName;
+        $this->filter = $filter;
+        $this->limit = $limit;
+        $this->offset = $offset;
+        $this->outputFields = $outputFields;
+        $this->dbName = $dbName;
+    }
+
+    protected function defaultBody(): array
+    {
+        return array_filter([
+            'collectionName' => $this->collectionName,
+            'filter' => $this->filter,
+            'limit' => $this->limit,
+            'offset' => $this->offset,
+            'outputFields' => $this->outputFields,
+            'dbName' => $this->dbName,
+        ]);
     }
 }
