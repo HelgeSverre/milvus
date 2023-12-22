9af017ee8c9c428ef8441ce459e4e2f22eb628e8
Helge Sverre <helge.sverre@gmail.com>
2023-12-22 15:06:12 +0100

    wip

--- src/Requests/VectorOperations/SearchVector.php
+++ src/Requests/VectorOperations/SearchVector.php
@@ -18,39 +18,21 @@ class SearchVector extends Request implements HasBody
 
     protected Method $method = Method::POST;
 
-    protected string $collectionName;
-    protected array $vector;
-    protected ?string $filter = null;
-    protected ?int $limit = null;
-    protected ?int $offset = null;
-    protected ?array $outputFields = null;
-    protected ?array $params = null;
-    protected ?string $dbName = null;
-
     public function resolveEndpoint(): string
     {
         return '/v1/vector/search';
     }
 
     public function __construct(
-        protected string $publicEndpoint, // The endpoint of your cluster.
-        string $collectionName,
-        array $vector,
-        ?string $filter = null,
-        ?int $limit = null,
-        ?int $offset = null,
-        ?array $outputFields = null,
-        ?array $params = null,
-        ?string $dbName = null
+        protected string $collectionName,
+        protected array $vector,
+        protected ?string $filter = null,
+        protected ?int $limit = null,
+        protected ?int $offset = null,
+        protected ?array $outputFields = null,
+        protected ?array $params = null,
+        protected ?string $dbName = null
     ) {
-        $this->collectionName = $collectionName;
-        $this->vector = $vector;
-        $this->filter = $filter;
-        $this->limit = $limit;
-        $this->offset = $offset;
-        $this->outputFields = $outputFields;
-        $this->params = $params;
-        $this->dbName = $dbName;
     }
 
     protected function defaultBody(): array
@@ -62,6 +44,8 @@ class SearchVector extends Request implements HasBody
             'limit' => $this->limit,
             'offset' => $this->offset,
             'outputFields' => $this->outputFields,
+
+            // TODO: restful api only supports "radius" and "range_filter at this time.
             'params' => $this->params,
             'dbName' => $this->dbName,
         ]);
