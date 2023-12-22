60f021ce1fceee922fa7bce99d44442c006bd2ad
Helge Sverre <helge.sverre@gmail.com>
2023-12-22 14:53:40 +0100

    aider: Implemented missing properties for `SearchVector` using the "/v1/vector/search" endpoint from `openapi.json`.
    # Aider chat conversation:
    
    USER: implement the missing properties in `src/Requests/VectorOperations/SearchVector.php` using the "/v1/vector/search" endpoint in the `openapi.json` speficication, reference `src/Requests/CollectionOperations/CreateCollection.php` to see how other similar files are implemented
    
    ASSISTANT: ```diff
    --- src/Requests/VectorOperations/SearchVector.php
    +++ src/Requests/VectorOperations/SearchVector.php
    @@ ... @@
     class SearchVector extends Request implements HasBody
     {
         use HasJsonBody;
    
         protected Method $method = Method::POST;
    
    +    protected string $collectionName;
    +    protected array $vector;
    +    protected ?string $filter = null;
    +    protected ?int $limit = null;
    +    protected ?int $offset = null;
    +    protected ?array $outputFields = null;
    +    protected ?array $params = null;
    +    protected ?string $dbName = null;
    +
         public function resolveEndpoint(): string
         {
             return '/v1/vector/search';
         }
    
         public function __construct(
    -        protected string $publicEndpoint,
    +        protected string $publicEndpoint, // The endpoint of your cluster.
    +        string $collectionName,
    +        array $vector,
    +        ?string $filter = null,
    +        ?int $limit = null,
    +        ?int $offset = null,
    +        ?array $outputFields = null,
    +        ?array $params = null,
    +        ?string $dbName = null
         ) {
    +        $this->collectionName = $collectionName;
    +        $this->vector = $vector;
    +        $this->filter = $filter;
    +        $this->limit = $limit;
    +        $this->offset = $offset;
    +        $this->outputFields = $outputFields;
    +        $this->params = $params;
    +        $this->dbName = $dbName;
         }
    +
    +    protected function defaultBody(): array
    +    {
    +        return array_filter([
    +            'collectionName' => $this->collectionName,
    +            'vector' => $this->vector,
    +            'filter' => $this->filter,
    +            'limit' => $this->limit,
    +            'offset' => $this->offset,
    +            'outputFields' => $this->outputFields,
    +            'params' => $this->params,
    +            'dbName' => $this->dbName,
    +        ]);
    +    }
     }
    ```

--- src/Requests/VectorOperations/SearchVector.php
+++ src/Requests/VectorOperations/SearchVector.php
@@ -18,13 +18,52 @@ class SearchVector extends Request implements HasBody
 
     protected Method $method = Method::POST;
 
+    protected string $collectionName;
+    protected array $vector;
+    protected ?string $filter = null;
+    protected ?int $limit = null;
+    protected ?int $offset = null;
+    protected ?array $outputFields = null;
+    protected ?array $params = null;
+    protected ?string $dbName = null;
+
     public function resolveEndpoint(): string
     {
         return '/v1/vector/search';
     }
 
     public function __construct(
-        protected string $publicEndpoint,
+        protected string $publicEndpoint, // The endpoint of your cluster.
+        string $collectionName,
+        array $vector,
+        ?string $filter = null,
+        ?int $limit = null,
+        ?int $offset = null,
+        ?array $outputFields = null,
+        ?array $params = null,
+        ?string $dbName = null
     ) {
+        $this->collectionName = $collectionName;
+        $this->vector = $vector;
+        $this->filter = $filter;
+        $this->limit = $limit;
+        $this->offset = $offset;
+        $this->outputFields = $outputFields;
+        $this->params = $params;
+        $this->dbName = $dbName;
+    }
+
+    protected function defaultBody(): array
+    {
+        return array_filter([
+            'collectionName' => $this->collectionName,
+            'vector' => $this->vector,
+            'filter' => $this->filter,
+            'limit' => $this->limit,
+            'offset' => $this->offset,
+            'outputFields' => $this->outputFields,
+            'params' => $this->params,
+            'dbName' => $this->dbName,
+        ]);
     }
 }
