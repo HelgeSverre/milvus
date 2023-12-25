65b0e10bf3256bd5ba50822f4a180eb550178097
Helge Sverre <helge.sverre@gmail.com>
2023-12-25 14:02:03 +0100

    wip

--- tests/Feature/CollectionTest.php
+++ tests/Feature/CollectionTest.php
@@ -10,7 +10,7 @@ it('confirms that listing collections in the db is empty', function () {
 
     $response = $this->milvus->collections()->list();
 
-    dd($response->json());
+//    dd($response->json());
 
 });
 
@@ -21,7 +21,48 @@ it('creates a collection and confirms the list method returns 1 item', function 
         dimension: 128,
     );
 
+    expect($response->json('code'))->toEqual(200);
+
+
+//    $response = $this->milvus->collections()->list();
+//    expect($response->json('data'))->toHaveCount(1);
+
+
+    $describe = $this->milvus->collections()->describe(collectionName: 'test_collection');
+    expect($describe->json('code'))->toEqual(200);
+    dd($describe->json());
+
+    $response = $this->milvus->collections()->drop(collectionName: 'test_collection');
+    expect($response->json('code'))->toEqual(200);
+
     $response = $this->milvus->collections()->list();
+    expect($response->json('data'))->toHaveCount(0);
+
+});
+
+it('can insert stuff into collections', function () {
+
+    $response = $this->milvus->collections()->create(
+        collectionName: 'add_stuff_into_collections',
+        dimension: 128,
+    );
+
+
+    $response = $this->milvus->vector()->insert(
+        collectionName: 'add_stuff_into_collections',
+        data: [
+            "vector" => array_fill(0, 128, 0.1),
+        ],
+    );
+
+    dump($response->json());
+
+    $response = $this->milvus->vector()->query(
+        collectionName: 'add_stuff_into_collections',
+        filter: "id in [446564440140681987]",
+    );
+
+
 
     dd($response->json());
 
